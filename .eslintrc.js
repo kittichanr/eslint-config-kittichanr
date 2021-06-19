c69c1785a0e5a959a35db4a66ed084325bff4e7f
petchkubbb <>
2021-06-19 09:13:01 +0700

    update 0.1.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,42 @@
 module.exports = {
+    root: true,
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    env: {
+        browser: true,
+        node: true,
+        commonjs: true,
+        es6: true,
+        'jest/globals': true,
+        jest: true
+    },
     globals: {
         MyGlobal: true
     },
-    plugins: ['react', 'jest', 'react-hooks'],
+    extends: [
+        'airbnb',
+        'eslint:recommended',
+        'plugin:import/errors',
+        'plugin:react/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:prettier/recommended',
+        'prettier/react',
+    ],
+    plugins: [
+        'react-hooks',
+    ],
     rules: {
-        semi: [2, "never"]
-    }
+        'semi': [2, 'never'],
+        // 'max-len': 'off',
+        // 'no-tabs': 'off',
+        // 'comma-dangle': 'off',
+        // indent: 'off',
+        'quotes': [2, 'single', 'avoid-escape'],
+    },
 };
